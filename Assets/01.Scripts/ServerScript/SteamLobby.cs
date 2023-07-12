e9ec9c5bc11a6bbcca225c7c395aa2900a4ebae5
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2023-07-13 00:55:48 +0900

    .

---
+++ Assets/01.Scripts/ServerScript/SteamLobby.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using Steamworks;
+using UnityEngine.UI;
+
+public class SteamLobby : MonoBehaviour
+{
+    protected Callback<LobbyCreated_t> LobbyCreated;
+    protected Callback<GameLobbyJoinRequested_t> JoinRequest;
+    protected Callback<LobbyEnter_t> LobbyEntered;
+
+    public ulong CurrentLobbyID;
+    private const string HostAddressKey = "HostAddress";
+    private CustomNetworkManager manager;
+
+    public GameObject HostButton;
+    public Text LobbyNameText;
+
+    private void OnLobbyCreated(LobbyCreated_t callback)
+    {
+        if(callback.m_eResult != EResult.k_EResultOK) { return; }
+        Debug.Log("Lobby created Successfully");
+
+        manager.StartHost();
+
+        SteamMatchmaking.SetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), HostAddressKey, SteamUser.GetSteamID().ToString());
+        SteamMatchmaking.SetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), "name", SteamFriends.GetPersonaName().ToString()+"`s LOBBY");
+    }
+}
