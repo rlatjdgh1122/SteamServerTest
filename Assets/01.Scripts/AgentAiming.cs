fe3e63b3092cf7463adaffbf76e3ad4b70a8496b
rlatjdgh1122 <rlatjdgh2358@naver.com>
2023-09-02 02:25:03 +0900

    카메라 시스템 완료 고정키 푸는거만 추가하면 댐 내일은 그랩시스템 제작

--- Assets/01.Scripts/AgentAiming.cs
+++ Assets/01.Scripts/AgentAiming.cs
@@ -9,12 +9,15 @@ public class AgentAiming : MonoBehaviour
     [SerializeField] private CameraController _cameraController;
     [SerializeField] private Transform _pivot;
 
-    private void LateUpdate()
+    private void Update()
     {
-        Vector3 pos = _cameraController.Cam.ScreenToWorldPoint(_inputReader.MousePostion);
-        Vector3 dir = (pos - transform.position).normalized;
-
-        Debug.Log("pos : " + pos + "dir :" + dir);
+        Vector3 screentToPlayerPos = _cameraController.Cam.WorldToScreenPoint(transform.position);
+        Vector3 dir = (
+                new Vector3(
+                _inputReader.MousePostion.x,
+                _inputReader.MousePostion.y,
+                0)
+                - screentToPlayerPos).normalized;
 
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         _pivot.rotation = Quaternion.Euler(0, 0, angle - 90);
--- Assets/01.Scripts/Object/Grab.cs
+++ Assets/01.Scripts/Object/Grab.cs
@@ -4,5 +4,5 @@ using UnityEngine;
 
 public class Grab : MonoBehaviour
 {
-
+    //만약 걸린다면 적의 움직임을 멈추고 플레이어도 시전시간동안 일시적으로 멈춤
 }
